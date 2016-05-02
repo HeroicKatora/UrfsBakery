735fca8a7b12cadcc502aa5a694dbb8f0e269ac4
WorldSEnder <worldsbegin@gmx.de>
2016-05-02 18:06:55 +0200

    Split css, added example page title

--- app/bakery.module.js
+++ app/bakery.module.js
@@ -4,4 +4,5 @@ var bakeryModule = angular.module('bakery', []);
 bakeryModule.run(['$rootScope', function($scope){
 	$scope.clicker = clicker;
 	$scope.clickerSetup = ClickerSetup;
+	$scope.pageTitle = 'example title';
 }]);
