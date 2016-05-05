b8f5ef284b8264691cf9003bde8a441f6816736d
WorldSEnder <worldsbegin@gmx.de>
2016-05-05 17:01:38 +0200

    Added basic support for menus

--- app/bakery.module.js
+++ app/bakery.module.js
@@ -1,8 +1,24 @@
 /* Setup, load and config angular */
 var bakeryModule = angular.module('bakery', []);
 
-bakeryModule.run(['$rootScope', function($scope){
+bakeryModule
+.factory('Game', ['$rootScope', function($scope) {
+	// Define a service, we later inject into global scope.
+	return {
+		menuPath: '',
+		openMenu: function(menu) {
+			if(menu == 'champions') {
+				this.menuPath = 'app/components/game/championMenu.htm'
+			} else {
+				console.log("missing menu " + menu);
+			}
+		}
+	};
+}])
+.run(['Game', '$rootScope', function(Game, $scope){
 	$scope.clicker = clicker;
 	$scope.clickerSetup = ClickerSetup;
 	$scope.pageTitle = 'example title';
+	// Inject the service
+	$scope.Bakery = Game;
 }]);
