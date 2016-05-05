e3460e7d91c180284a83b2703d4793a6d8b12587
WorldSEnder <worldsbegin@gmx.de>
2016-05-05 18:32:42 +0200

    Split game controller (service)

--- app/bakery.module.js
+++ app/bakery.module.js
@@ -2,23 +2,7 @@
 "use strict";
 var bakeryModule = angular.module('bakery', []);
 
-bakeryModule
-.factory('Game', ['$rootScope', function($scope) {
-	// Define a service, we later inject into global scope.
-	return {
-		menuPath: '',
-		openMenu: function(menu) {
-			if(menu == 'champions') {
-				this.menuPath = 'app/components/game/championMenu.htm';
-			} else if(menu == '') {
-				this.menuPath = '';
-			} else {
-				console.log("missing menu " + menu);
-			}
-		}
-	};
-}])
-.run(['Game', '$rootScope', function(Game, $scope){
+bakeryModule.run(['Game', '$rootScope', function(Game, $scope){
 	$scope.clicker = clicker;
 	$scope.clickerSetup = ClickerSetup;
 	$scope.pageTitle = 'example title';
